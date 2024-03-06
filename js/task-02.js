78a1ddbf56f74a1edbc19a2ff3609732fcdf6707
AgnieszkaWIT <151778905+AgnieszkaWIT@users.noreply.github.com>
2024-03-06 23:42:31 +0100

    zadanie 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientRoot = document.querySelector('#ingredients');
+ingredients.forEach(ingredient => {
+  const ingredientItem = document.createElement("li");
+  ingredientItem.textContent = ingredient;
+  ingredientItem.classList.add("item");
+  ingredientRoot.appendChild(ingredientItem);
+});
