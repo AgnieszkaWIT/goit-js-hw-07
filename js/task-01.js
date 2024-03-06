58f6f044df28707daa1f6e3b3341b033e5766ad1
AgnieszkaWIT <151778905+AgnieszkaWIT@users.noreply.github.com>
2024-03-06 23:24:40 +0100

    zadanie 1

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const categoriesRoot = document.querySelector('#categories');
+const categories = categories.querySelectorAll(".item");
+console.log("Number of items: " + categories.length);
+
+categories.forEach(category => {
+    const categoryName = category.querySelector("h2").textContent;
+    const categoryElementsNumber = category.querySelectorAll("ul > li").length;
+    console.log("Category: " + categoryName);
+    console.log("Elements: " + categoryElementsNumber);
+});
