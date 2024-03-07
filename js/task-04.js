afd846a942891ed61628a566ed614e3dc83a7021
AgnieszkaWIT <151778905+AgnieszkaWIT@users.noreply.github.com>
2024-03-07 02:11:58 +0100

    zadanie 4 addBox

--- js/task-04.js
+++ js/task-04.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const firstBoxSize = 30;
+let lastBoxSize = 0;
+
+document.querySelector("[data-create]").addEventListener("click", () => {
+  amount = document.querySelector("input").value;
+  createBoxes(amount);
+});
+
+document.querySelector("[data-destroy]").addEventListener("click", () => {
+  destroyBoxes();
+});
+
+function createBoxes(amount) {
+  const boxesRoot = document.getElementById("boxes");
+
+  for (let i=0; i<amount; i++){
+    const boxItem = document.createElement("div");
+    boxItem.style.width = "30px";
+    boxItem.style.height = `${lastBoxSize > firstBoxSize ? lastBoxSize : firstBoxSize}px`;
+    boxItem.style.backgroundColor =  getRandomHexColor();
+    boxItem.classList.add("box-item");
+    boxesRoot.appendChild(boxItem);
+    lastBoxSize = (lastBoxSize > firstBoxSize ? lastBoxSize : firstBoxSize) + 10;
+  }
+
+}
+
+function destroyBoxes() {
+  
+}
